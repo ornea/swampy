0c4152156bc9366f78f6e9df58d7f8df0a695455
ornea <justin.richards@gmail.com>
2015-11-03 17:54:11 +0800

    Update init.lua
    ESP8266 would not accept DHCP offer from BILLION Router so set to Static

--- init.lua
+++ init.lua
@@ -6,16 +6,24 @@ Date: 28/Oct/2015
 Title: Init.lua
 --]]
 
-
 --init.lua
 print("Setting up WIFI...")
 wifi.setmode(wifi.STATION)
+cfg =
+  {
+    ip="192.168.1.51",
+    netmask="255.255.255.0",
+    gateway="192.168.1.254"
+  }
+  wifi.sta.setip(cfg)
+
 --modify according your wireless router settings
 
 --wifi.sta.connect()
 tmr.alarm(1, 1000, 1, function() 
+  wifi.sta.config("SSID","password",0)
 if wifi.sta.getip()== nil then 
-     wifi.sta.config("test","test")
+   
      print("IP unavaiable, Waiting...") 
 else 
      tmr.stop(1)
@@ -23,3 +31,5 @@ else
      dofile("Swampy.lua")
 end 
 end)
+--print(wifi.sta.getmac())
+--wifi.sta.connect()
